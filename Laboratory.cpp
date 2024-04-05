39ae9a9f85fe2a971b34e047c34ed777dc73579d
Ethan Warren <ewarren5@rockets.utoledo.edu>
2024-04-05 15:44:10 -0400

    Add files via upload

---
+++ Laboratory.cpp
@@ -0,0 +1,26 @@
+/*
+ * Program:  <First Laboratory>
+ * Author:   <Ethan Warren>
+ * Date:     <1/25/24>
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) 
+{
+	double width;
+	double length;
+	double area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle area = " << area << endl;
+
+
+	cout << "Who's on first?" << endl;
+	return 0;
+}
